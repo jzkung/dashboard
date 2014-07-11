9d6e86a302096d55edd5dbfdc7f9d28fb1dedb16
Jessica Kung <jzkung@gmail.com>
2014-07-11 11:27:00 -0700

    trying out gemstore application using mean

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -1,9 +1,19 @@
 'use strict';
 
-
+var gems = [
+	{
+		name: 'Azurite',
+		price: 2
+	},
+	{
+		name: 'Bloodstone',
+		price: 5
+	}
+];
 angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 	function($scope, Authentication) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+		this.products = gems;
 	}
 ]);
