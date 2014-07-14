b3550ac99eb41efe7398df1fdbd88be8a1488c12
Joseph, Sunil <Sunil_Joseph@intuit.com>
2014-07-14 18:53:38 -0500

    Built the application

--- public/dist/application.js
+++ public/dist/application.js
@@ -108,14 +108,157 @@ angular.module('core').controller('HeaderController', [
     });
   }
 ]);'use strict';
+var gems = [
+    {
+      name: 'Azurite',
+      description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.',
+      shine: 8,
+      price: 110.5,
+      rarity: 7,
+      color: '#CCC',
+      faces: 14,
+      images: [
+        'images/gem-02.gif',
+        'images/gem-05.gif',
+        'images/gem-09.gif'
+      ],
+      reviews: [
+        {
+          stars: 5,
+          body: 'I love this gem!',
+          author: 'joe@example.org',
+          createdOn: 1397490980837
+        },
+        {
+          stars: 1,
+          body: 'This gem sucks.',
+          author: 'tim@example.org',
+          createdOn: 1397490980837
+        }
+      ]
+    },
+    {
+      name: 'Bloodstone',
+      description: 'Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.',
+      shine: 9,
+      price: 22.9,
+      rarity: 6,
+      color: '#EEE',
+      faces: 12,
+      images: [
+        'images/gem-01.gif',
+        'images/gem-03.gif',
+        'images/gem-04.gif'
+      ],
+      reviews: [
+        {
+          stars: 3,
+          body: 'I think this gem was just OK, could honestly use more shine, IMO.',
+          author: 'JimmyDean@example.org',
+          createdOn: 1397490980837
+        },
+        {
+          stars: 4,
+          body: 'Any gem with 12 faces is for me!',
+          author: 'gemsRock@example.org',
+          createdOn: 1397490980837
+        }
+      ]
+    },
+    {
+      name: 'Zircon',
+      description: 'Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.',
+      shine: 70,
+      price: 1100,
+      rarity: 2,
+      color: '#000',
+      faces: 6,
+      images: [
+        'images/gem-06.gif',
+        'images/gem-07.gif',
+        'images/gem-08.gif'
+      ],
+      reviews: [
+        {
+          stars: 1,
+          body: 'This gem is WAY too expensive for its rarity value.',
+          author: 'turtleguyy@example.org',
+          createdOn: 1397490980837
+        },
+        {
+          stars: 1,
+          body: 'BBW: High Shine != High Quality.',
+          author: 'LouisW407@example.org',
+          createdOn: 1397490980837
+        },
+        {
+          stars: 1,
+          body: 'Don\'t waste your rubles!',
+          author: 'nat@example.org',
+          createdOn: 1397490980837
+        }
+      ]
+    }
+  ];
 angular.module('core').controller('HomeController', [
   '$scope',
   'Authentication',
   function ($scope, Authentication) {
     // This provides Authentication context.
     $scope.authentication = Authentication;
+    this.products = gems;
   }
 ]);'use strict';
+angular.module('core').controller('ReviewController', [
+  '$scope',
+  function ($scope) {
+    this.review = [];
+    this.addReview = function (product) {
+      product.reviews.push(this.review);
+      this.review = [];
+    };
+  }
+]);'use strict';
+angular.module('core').directive('productDescription', [function () {
+    return {
+      restrict: 'E',
+      templateUrl: 'modules/core/views/product-description.client.view.html'
+    };
+  }]);'use strict';
+angular.module('core').directive('productReviews', [function () {
+    return {
+      restrict: 'E',
+      templateUrl: 'modules/core/views/product-reviews.client.view.html'
+    };
+  }]);'use strict';
+angular.module('core').directive('productSpecs', [function () {
+    return {
+      restrict: 'E',
+      templateUrl: 'modules/core/views/product-specs.client.view.html'
+    };
+  }]);'use strict';
+angular.module('core').directive('productTabs', [function () {
+    return {
+      restrict: 'E',
+      templateUrl: 'modules/core/views/product-tabs.client.view.html',
+      controller: function () {
+        this.tab = 1;
+        this.setTab = function (setTab) {
+          this.tab = setTab;
+        };
+        this.isSet = function (checkTab) {
+          return this.tab === checkTab;
+        };
+      },
+      controllerAs: 'tab'
+    };
+  }]);'use strict';
+angular.module('core').directive('productTitle', [function () {
+    return {
+      restrict: 'E',
+      templateUrl: 'modules/core/views/product-title.client.view.html'
+    };
+  }]);'use strict';
 //Menu service used for managing  menus
 angular.module('core').service('Menus', [function () {
     // Define a set of default roles
