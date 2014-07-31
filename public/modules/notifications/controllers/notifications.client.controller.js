82cad38f7a67418b4430d948d64dcfc68ff0e4ea
Joseph, Sunil <Sunil_Joseph@intuit.com>
2014-07-31 10:44:02 -0500

    Change double quotes to single quotes

--- public/modules/notifications/controllers/notifications.client.controller.js
+++ public/modules/notifications/controllers/notifications.client.controller.js
@@ -2,26 +2,26 @@
 
 var alerts = [
 	{
-		title: "Finish Performance Review",
-		link: "http://www.workday.com",
-		dueDate: "tomorrow"
+		title: 'Finish Performance Review',
+		link: 'http://www.workday.com',
+		dueDate: 'tomorrow'
 	},
 	{
-		title: "Submit fitness reimbursement",
-		link: "http://www.workday.com",
-		dueDate: "7/28/2014"
+		title: 'Submit fitness reimbursement',
+		link: 'http://www.workday.com',
+		dueDate: '7/28/2014'
 	},
 	{
-		title: "Scheduled maintenance",
-		link: "http://www.workday.com",
-		dueDate: "today"
+		title: 'Scheduled maintenance',
+		link: 'http://www.workday.com',
+		dueDate: 'today'
 	}
 ];
 
 angular.module('notifications').controller('NotificationsController', ['$scope',
 	function($scope) {
 		$scope.widget = {
-			title: "My Notifications"
+			title: 'My Notifications'
 		};
 		this.alerts = alerts;
 	}
