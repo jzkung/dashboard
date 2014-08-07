a4d234793d61513c07b8fe5a9951d30df751d2d9
shwetarameshk <skumar34@MTVL121d4cede.local>
2014-08-07 14:37:43 -0700

    Updated server methods

--- app/controllers/teams.server.controller.js
+++ app/controllers/teams.server.controller.js
@@ -4,6 +4,7 @@
  * Module dependencies.
  */
  var request = require('superagent');
+ var moment = require('moment');
 //var agent = superagent.agent();
 
 var checkBirthdayToday = function (birth_date){
@@ -54,13 +55,26 @@ var calculateNumAnniversary = function (hire_date){
 	}
 };
 
+var getISOStringFromDate = function (date_info){
+
+	if (date_info){
+		return new Date(date_info.replace('-','/')).toISOString();
+	}
+	else {
+		return null;
+	}
+};
+
+var getDisplayDate = function (date_info){
+
+};
+
 exports.getInfoFromServer = function (req, res){
 	var managerId = req.params.managerId;
 	var url = 'http://localhost:8080/workerService/ws/api/v1/workers/' + managerId + '/directReports';
 	request
 	.get(url)
 	.end(function(response){
-		console.log('server method');
 		var workerInfo = [];
 		if (response.error) {
 			console.log('error is ' + response.error.message);
@@ -74,10 +88,10 @@ exports.getInfoFromServer = function (req, res){
 				workerInfo.push ({
 					name: data.WORKER_NAMES[0].FIRST_NAME + ' ' + data.WORKER_NAMES[0].LAST_NAME,
 					title: data.BUSINESS_TITLE,
-					birthday: data.BIRTH_DATE,
-					hireDate: data.HIRE_DATE,
+					birthday: getISOStringFromDate(data.BIRTH_DATE),
+					hireDate: getISOStringFromDate(data.HIRE_DATE),
 					link: 'http://www.workday.com',
-					image: 'modules/team/img/jessicar.png', //TODO: modify image field
+					image: 'modules/team/img/user-icon.png', 
 					isBirthdayToday: checkBirthdayToday(data.BIRTH_DATE), 
 					isAnniversaryToday: checkAnniversaryToday(data.HIRE_DATE),
 					numAnniversary: calculateNumAnniversary(data.HIRE_DATE)
@@ -87,24 +101,24 @@ exports.getInfoFromServer = function (req, res){
 			workerInfo.push ({
 				name: 'shweta',
 				title: 'a',
-				birthday: '1988-08-05',
-				hireDate: '2001-08-05',
+				birthday: getISOStringFromDate('1988-08-07'),
+				hireDate: getISOStringFromDate('2001-08-07'),
 				link: 'http://www.workday.com',
-				image: 'modules/team/img/jessicar.png', //TODO: modify image field
-				isBirthdayToday: checkBirthdayToday('1988-08-05'), 
-				isAnniversaryToday: checkAnniversaryToday('2001-08-05'),
-				numAnniversary: calculateNumAnniversary('2001-08-05')
+				image: 'modules/team/img/user-icon.png',
+				isBirthdayToday: checkBirthdayToday('1988-08-07'), 
+				isAnniversaryToday: checkAnniversaryToday('2001-08-07'),
+				numAnniversary: calculateNumAnniversary('2001-08-07')
 			});
 			workerInfo.push ({
 				name: 'anagha',
 				title: 'a',
-				birthday: '2000-08-05',
-				hireDate: '2001-08-05',
+				birthday: getISOStringFromDate('1988-08-05'),
+				hireDate: getISOStringFromDate('2001-08-07'),
 				link: 'http://www.workday.com',
-				image: 'modules/team/img/jessicar.png', //TODO: modify image field
+				image: 'modules/team/img/user-icon.png',
 				isBirthdayToday: checkBirthdayToday('1988-08-05'), 
 				isAnniversaryToday: checkAnniversaryToday('2001-08-07'),
-				numAnniversary: calculateNumAnniversary('2001-08-05')
+				numAnniversary: calculateNumAnniversary('2001-08-07')
 			});
 			/* end of test data */
 		}
