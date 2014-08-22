a0e578000817be994456aae0d7c9f02b573e1604
RMathews <rmathews@MTVL122b7c8d4.local>
2014-08-22 15:05:40 -0700

    Update

--- app/controllers/notifications.server.controller.js
+++ app/controllers/notifications.server.controller.js
@@ -45,7 +45,7 @@
  exports.list = function(req, res, next) {
 
  	var dismissedNotifs = [];
- 	console.log('Users uname is : '+ req.user.username);
+ 	//console.log('Users uname is : '+ req.user.username);
  	User.findOne({'username': req.user.username}, 'dismissedNotificationIds',function (err, notifications) {
  		if (err) {
  			return next(err);
@@ -54,31 +54,35 @@
  			dismissedNotifs = notifications.dismissedNotificationIds;
  			console.log(dismissedNotifs);
  		}
+ 		if(notifications === null){
+ 			return res.json(204, {message: 'No user by this username'});
+ 		}
+ 		else{
+ 			Notification.find({$and:[{$or:[{'startDate':{$lte:new Date ()}},{'startDate':null}]},{$or:[{'endDate':{$gte:new Date ()}},{'endDate':null}]},{'_id':{$nin:dismissedNotifs}}]},function (err, notifications) {
+ 				if (err) {
+ 					return next(err);
+ 				}
+ 				var notificationResult = [];
 
- 		Notification.find({$and:[{$or:[{'startDate':{$lte:new Date ()}},{'startDate':null}]},{$or:[{'endDate':{$gte:new Date ()}},{'endDate':null}]},{'_id':{$nin:dismissedNotifs}}]},function (err, notifications) {
- 			if (err) {
- 				return next(err);
- 			}
- 			var notificationResult = [];
+ 				_.forEach (notifications, function (notification){
 
- 			_.forEach (notifications, function (notification){
+ 					notificationResult.push ({
+ 						Id: notification._id,
+ 						title: notification.title,
+ 						description: notification.description,
+ 						links: notification.link,
+ 						startDate: notification.startDate,
+ 						endDate: notification.endDate
+ 					});
 
- 				notificationResult.push ({
- 					Id: notification._id,
- 					title: notification.title,
- 					description: notification.description,
- 					links: notification.link,
- 					startDate: notification.startDate,
- 					endDate: notification.endDate
  				});
-
+ 				console.log(notifications);
+ 				console.log('EXPORTS');
+ 				return res.json(200, notificationResult);
  			});
- 			console.log(notifications);
- 			console.log('EXPORTS');
- 			return res.json(200, notificationResult);
- 		});
+ 		}
  	});
- };
+};
 
  //db.notifications.find({$and:[{$or:[{'startDate':{$lte:new Date ()}},{'startDate':null}]},{$or:[{'endDate':{$gte:new Date ()}},{'endDate':null}]},{"_id":{$nin:[ObjectId("53ed0d34337119bc1a922fdd")]}}]});
 
