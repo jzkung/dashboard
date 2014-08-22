e499028b9ea4ca12c09d85a861307f4ed3e13959
RMathews <rmathews@MTVL122b7c8d4.local>
2014-08-22 15:18:28 -0700

    Revert "Update"
    This reverts commit a0e578000817be994456aae0d7c9f02b573e1604.

--- app/tests/notification.server.model.test.js
+++ app/tests/notification.server.model.test.js
@@ -3,75 +3,53 @@
 /**
  * Module dependencies.
  */
- var should = require('should'),
- mongoose = require('mongoose'),
- User = mongoose.model('User'),
- Notification = mongoose.model('Notification');
- var request = require('superagent');
- 
+var should = require('should'),
+	mongoose = require('mongoose'),
+	User = mongoose.model('User'),
+	Notification = mongoose.model('Notification');
+
 /**
  * Globals
  */
- var user, notification;
+var user, notification;
 
 /**
  * Unit tests
  */
- describe('Notification Model Unit Tests:', function() {
- 	beforeEach(function(done) {
- 		user = new User({
- 			firstName: 'Full',
- 			lastName: 'Name',
- 			displayName: 'Full Name',
- 			email: 'test@test.com',
- 			username: 'username',
- 			password: 'password'
- 		});
+describe('Notification Model Unit Tests:', function() {
+	beforeEach(function(done) {
+		user = new User({
+			firstName: 'Full',
+			lastName: 'Name',
+			displayName: 'Full Name',
+			email: 'test@test.com',
+			username: 'username',
+			password: 'password'
+		});
 
- 		user.save(function() { 
- 			notification = new Notification({
+		user.save(function() { 
+			notification = new Notification({
 				// Add model fields
 				// ...
 			});
 
- 			done();
- 		});
- 	});
+			done();
+		});
+	});
 
- 	describe('Method Save', function() {
- 		it('should be able to save without problems', function(done) {
- 			return notification.save(function(err) {
- 				should.not.exist(err);
- 				done();
- 			});
- 		});
- 	});
+	describe('Method Save', function() {
+		it('should be able to save without problems', function(done) {
+			return notification.save(function(err) {
+				should.not.exist(err);
+				done();
+			});
+		});
+	});
 
- 	describe('Get notifications for the logged in user', function(){
- 		it('should respond with json', function(done){
- 			request.get('http://localhost:3000/api/notifications')
- 			.end(function(response){
- 				response.status.should.equal(200);
- 				response.should.have.header('Content-Type','/json');
- 			});
- 			done();
- 		});
- 		// it('should have title field', function(done){
- 		// 	request.get.('http://localhost:3000/api/notifications')
- 		// 	.end(function(response){
- 		// 		response.status.should.equal(200);
- 		// 		response.body.should.have.property('title');
- 		// 	});
- 		// 	done();
- 		// });
- 	});
+	afterEach(function(done) { 
+		Notification.remove().exec();
+		User.remove().exec();
 
-
-
- 	afterEach(function(done) { 
- 		Notification.remove().exec();
- 		User.remove().exec();
-
- 		done();
- 	});
- });
+		done();
+	});
+});
